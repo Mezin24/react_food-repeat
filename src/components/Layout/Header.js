8fd1f6ce91a69b6fb5ee2aa5257a7a4028ca89da
Pavel Mezencev <mezencev24@mail.ru>
2022-08-19 09:21:35 +0300

    add header button cmp

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,3 +1,5 @@
+import HeaderButton from './HeaderButton';
+
 import mainImg from '../../assets/img/meals.jpg';
 
 import classes from './Header.module.css';
@@ -7,7 +9,7 @@ const Header = (props) => {
     <>
       <header className={classes.header}>
         <h1>React Meals</h1>
-        <button>Cart</button>
+        <HeaderButton />
       </header>
       <div className={classes['main-image']}>
         <img src={mainImg} alt='A table fool of tasty food' />
